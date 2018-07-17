e24a0eb7090eb1ec460182497050893ed858232f
chazmcgrill <charlietaylordev@gmail.com>
2018-07-17 15:28:26 +0100

    Improved Gulp Setup / Added Pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,49 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const imagemin = require("gulp-imagemin");
-const uglify = require("gulp-uglify");
+const uglify = require("gulp-uglify-es").default;
 const concat = require("gulp-concat");
+const pug = require('gulp-pug');
 
-gulp.task("default", defaultTask);
-
-function defaultTask(done) {
-  console.log("gulp is running...");
-  done();
+const paths = {
+  sass: ['./src/css/**/*.sass'],
+  pug: ['./src/**/*.pug'],
+  js: ['./src/js/**/*.js']
 }
 
-gulp.task('copyHtml', () => {
-  return gulp.src('src/*.html')
+gulp.task('templates', () => {
+  return gulp.src('./src/**/!(_)*.pug')
+    .pipe(pug({ pretty: true }))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', () => {
-  return gulp.src('src/css/*.sass')
+  return gulp.src('./src/css/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('scripts', function () {
-  gulp.src('src/js/*.js')
+gulp.task('scripts', () => {
+  return gulp.src('src/js/*.js')
     .pipe(uglify())
     .pipe(concat('app.js'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('imageMin', () =>
+gulp.task('imageMin', () => {
   gulp.src('src/img/*')
     .pipe(imagemin([imagemin.jpegtran({ progressive: true })]))
     .pipe(gulp.dest('dist/img'))
-);
-
-gulp.task('watch', function () {
-  gulp.watch("src/*.html", gulp.series("copyHtml"));
-  gulp.watch("src/js/*.js", gulp.series("scripts"));
-  gulp.watch("src/img/*", gulp.series("imageMin"));
-  gulp.watch("src/css/*.sass", gulp.series("sass"));
 });
+
+gulp.task('watch', () => {
+  gulp.watch(paths.pug, gulp.series("templates"));
+  gulp.watch("src/js/**/*.js", gulp.series("scripts"));
+  gulp.watch("src/img/*", gulp.series("imageMin"));
+  gulp.watch(paths.sass, gulp.series("sass"));
+});
+
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel('sass', 'templates', 'scripts', 'watch'))
+);
